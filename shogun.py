817deaf61660983f5acfced8c1de634fd6a6133b
venomsnake <thakurmark07@gmail.com>
2023-08-11 10:51:41 +0530

    pydantic version change

--- shogun.py
+++ shogun.py
@@ -1,14 +1,14 @@
 import asyncio
 import genshin
-try:
-    async def main():
-        cookies = {"ltuid": 215584138, "ltoken": "xhPj1YJ1A751yIzTb4Tc63OWf0UZiykFac5pp5K0"}
-        client = genshin.Client()
-        client.set_cookies_auto()
 
-        data = await client.get_genshin_user(857107919)
-        print("You have a total of {data.stats.characters} characters")
+async def main():
+    cookies = {"ltuid":ownkey, "ltoken":"owntokken"}
+    client = genshin.Client()
+    
+    skip_on_failure=True
+    uid = 
+    data = await client.get_genshin_user(uid)
+    print("You have a total of {data.stats.characters} characters")
 
-    asyncio.run(main())
-except ValidationError as e:
-    print(e)
+asyncio.run(main())
+
