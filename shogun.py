31159b3e5daaa44d51daf8f7fb003c6f3e4afd8e
Kiryu <98075959+kiryukazuma28@users.noreply.github.com>
2023-08-12 12:32:57 +0530

    Update shogun.py

--- shogun.py
+++ shogun.py
@@ -4,11 +4,20 @@ import genshin
 async def main():
     cookies = {"ltuid":ownkey, "ltoken":"owntokken"}
     client = genshin.Client()
+    client.set_browser_cookies()
     
     skip_on_failure=True
-    uid = 
-    data = await client.get_genshin_user(uid)
-    print("You have a total of {data.stats.characters} characters")
+    uid= int(input("Enter your UID : "))
+    user = await client.get_full_genshin_user(uid)
+        
+    print("Username :  ",user.info.nickname)
+    print("Characters : ",user.stats.characters)
+    print("Days Active : ",user.stats.days_active)
+    print("Acievements : ",user.stats.achievements)
+    print("Anemoculus Collected",user.stats.anemoculi)
+    print("Geoculus Collected",user.stats.geoculi)
+    print("Electroculus Collected ",user.stats.electroculi)
+    print("Dendroculus Collected",user.stats.dendroculi)
 
 asyncio.run(main())
 
